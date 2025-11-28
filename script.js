5803a5ed5b918e0f817e88ff163bfdb72bd94318
myathtetko <myathtetko7@gmail.com>
2025-11-28 14:55:32 +0630

    V3

--- script.js
+++ script.js
@@ -19,30 +19,4 @@ if (yearEl) {
   yearEl.textContent = new Date().getFullYear();
 }
 
-const slides = Array.from(document.querySelectorAll(".carousel .slide"));
-const prevBtn = document.querySelector(".carousel-btn.prev");
-const nextBtn = document.querySelector(".carousel-btn.next");
-let slideIndex = 0;
-
-const setSlide = index => {
-  slides.forEach((slide, i) => {
-    slide.classList.toggle("active", i === index);
-  });
-};
-
-const showNext = () => {
-  slideIndex = (slideIndex + 1) % slides.length;
-  setSlide(slideIndex);
-};
-
-const showPrev = () => {
-  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-  setSlide(slideIndex);
-};
-
-if (slides.length) {
-  nextBtn?.addEventListener("click", showNext);
-  prevBtn?.addEventListener("click", showPrev);
-  setInterval(showNext, 6000);
-}
 
